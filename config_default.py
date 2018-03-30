6c1bac6005b8522d10e4adac45247b851e0cb6a6
xRedSing <86536306@qq.com>
2018-03-30 16:45:34 +0800

    Add files via upload

---
+++ config_default.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'red',
+        'password': '123456',
+        'db': 'redtime'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ schema.sql
@@ -0,0 +1,47 @@
+-- schema.sql
+
+drop database if exists redtime;
+
+create database redtime;
+
+use redtime;
+
+grant select, insert, update, delete on redtime.* to 'red'@'localhost' identified by '123456';
+
+create table users (
+    `id` varchar(50) not null,
+    `email` varchar(50) not null,
+    `name` varchar(50) not null,
+    `passwd` varchar(50) not null,
+    `admin` bool not null,
+    `image` varchar(500) not null,
+    `created_at` real not null,
+    unique key `idx_email` (`email`),
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table blogs (
+    `id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `name` varchar(50) not null,
+    `summary` varchar(200) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table comments (
+    `id` varchar(50) not null,
+    `blog_id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
